7ae8deb6ae4f01a7fae37dbdbec856f12401cfb5
Kaushik <akaushikarkar@gmail.com>
2024-05-08 01:44:00 +0530

    add some utility files and setup express

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,16 @@
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 import dotenv from "dotenv";
-dotenv.config()
+dotenv.config();
 
-connectDB();
+const port = process.env.PORT || 6969;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log("Server listening on port", port);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed!!!", err);
+  });
