10fe3494d0c0e7196cc864c1be22a5fb39dc54fd
Kaushik <akaushikarkar@gmail.com>
2024-06-08 22:11:12 +0530

    made createTweet and getUserTweets endpoints

--- backend/src/controllers/tweet.controller.js
+++ backend/src/controllers/tweet.controller.js
@@ -6,11 +6,71 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
-  //TODO: create tweet
+  const { content } = req.body;
+
+  if (!content?.length) {
+    throw new ApiError(400, "Please send tweet content");
+  }
+
+  const tweet = await Tweet.create({ owner: req.user._id, content });
+
+  if (!tweet) {
+    throw new ApiError(500, "Error creating a new tweet");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "Tweet created successfully"));
 });
 
 const getUserTweets = asyncHandler(async (req, res) => {
-  // TODO: get user tweets
+  // TODO: Paginate
+
+  const { userId } = req.params;
+
+  if (!userId) {
+    throw new ApiError(400, "Please provide user id");
+  }
+
+  const tweets = await Tweet.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              fullName: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        owner: {
+          $first: "$owner",
+        },
+      },
+    },
+  ]);
+
+  if (!tweets) {
+    throw new ApiError(404, "No tweets found for this user");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweets, "Tweets fetched successfully"));
 });
 
 const updateTweet = asyncHandler(async (req, res) => {
@@ -21,4 +81,8 @@ const deleteTweet = asyncHandler(async (req, res) => {
   //TODO: delete tweet
 });
 
-export { createTweet, getUserTweets, updateTweet, deleteTweet };
+const getAllTweets = asyncHandler(async (req, res) => {
+  //TODO: get tweets
+});
+
+export { createTweet, getUserTweets, updateTweet, deleteTweet, getAllTweets };
