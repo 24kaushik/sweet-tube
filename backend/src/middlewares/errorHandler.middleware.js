13e7c68701f25784b067c97e2e0a20340d05c201
Kaushik <akaushikarkar@gmail.com>
2024-06-10 22:02:31 +0530

    added healthCheck route and improved error handling

--- backend/src/middlewares/errorHandler.middleware.js
+++ backend/src/middlewares/errorHandler.middleware.js
@@ -2,12 +2,28 @@ import { ApiError } from "../utils/ApiError.js";
 
 export const errorHandler = (err, req, res, next) => {
   if (err instanceof ApiError) {
-    res.status(err.statusCode).json({
+    const response = {
       statusCode: err.statusCode,
       message: err.message,
       success: err.success,
-    });
-  } else {
+    };
+
+    if (
+      process.env.ENVIRONMENT == "dev" ||
+      process.env.ENVIRONMENT == "development"
+    ) {
+      response.stack = err.stack;
+    }
+
+    res.status(err.statusCode).json(response);
+  } else {    
+    if (
+      process.env.ENVIRONMENT == "dev" ||
+      process.env.ENVIRONMENT == "development"
+    ) {
+      console.log(err);
+    }
+    
     res.status(500).json({
       statusCode: 500,
       message: "Internal server error!",
