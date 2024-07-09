65ed8b719ee729b8a2bca65771695d36b131c8e7
Kaushik <akaushikarkar@gmail.com>
2024-07-09 13:56:09 +0530

    add optional login middleware

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -35,5 +35,31 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
 
 export const optionalVerifyJWT = asyncHandler(async (req, res, next) => {
   // TODO: optionally log in user if user sends auth token
-  next()
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      return next();
+    }
+
+    const decodedToken = await jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      return next();
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
+  }
 });
