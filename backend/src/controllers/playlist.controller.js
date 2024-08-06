c27a387c3184e89f3934187628e2afd3446702c5
Kaushik <akaushikarkar@gmail.com>
2024-08-06 19:07:28 +0530

    added an endpoint to add videos into playlist

--- backend/src/controllers/playlist.controller.js
+++ backend/src/controllers/playlist.controller.js
@@ -1,5 +1,6 @@
 import mongoose, { isValidObjectId } from "mongoose";
 import { Playlist } from "../models/playlist.model.js";
+import { Video } from "../models/video.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -52,7 +53,31 @@ const getPlaylistById = asyncHandler(async (req, res) => {
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
   const { playlistId, videoId } = req.params;
-  //TODO
+  if (!(isValidObjectId(playlistId) && isValidObjectId(videoId))) {
+    throw new ApiError(400, "Please provide a valid playlist id and video id");
+  }
+
+  const playlist = await Playlist.findById(playlistId);
+  if (!playlist) {
+    throw new ApiError(404, "Playlist does not exists");
+  }
+  if (!playlist.owner.equals(req.user._id)) {
+    throw new ApiError(401, "You are not the owner of this playlist");
+  }
+
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(404, "Video does not exists");
+  }
+  if (playlist.videos.find((v) => v.equals(video._id)) !== undefined) {
+    throw new ApiError(400, "Video already exists in playlist")
+  }
+
+  playlist.videos.unshift(video._id);
+  await playlist.save({ validateBeforeSave: false });
+  return res
+    .status(200)
+    .json(new ApiResponse(200, playlist, "Video added successfully"));
 });
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
