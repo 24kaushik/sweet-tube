5464420fde85e430130bf8d72f61d42cc36bced2
Kaushik <akaushikarkar@gmail.com>
2024-08-06 19:25:09 +0530

    added an endpoint to remove videos from playlist

--- backend/src/controllers/playlist.controller.js
+++ backend/src/controllers/playlist.controller.js
@@ -70,7 +70,7 @@ const addVideoToPlaylist = asyncHandler(async (req, res) => {
     throw new ApiError(404, "Video does not exists");
   }
   if (playlist.videos.find((v) => v.equals(video._id)) !== undefined) {
-    throw new ApiError(400, "Video already exists in playlist")
+    throw new ApiError(400, "Video already exists in playlist");
   }
 
   playlist.videos.unshift(video._id);
@@ -83,6 +83,32 @@ const addVideoToPlaylist = asyncHandler(async (req, res) => {
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
   const { playlistId, videoId } = req.params;
   // TODO: remove video from playlist
+  if (!(isValidObjectId(playlistId) && isValidObjectId(videoId))) {
+    throw new ApiError(400, "Please provide a valid playlist id and video id");
+  }
+
+  const playlist = await Playlist.findById(playlistId);
+  if (!playlist) {
+    throw new ApiError(404, "Playlist does not exists");
+  }
+  if (!playlist.owner.equals(req.user._id)) {
+    throw new ApiError(401, "You are not the owner of this playlist");
+  }
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(404, "Video does not exists");
+  }
+
+  const videoIndex = playlist.videos.findIndex((v) => v.equals(video._id));
+  if (videoIndex === -1) {
+    throw new ApiError(400, "Video does not exists in playlist");
+  }
+
+  playlist.videos.splice(videoIndex, 1);
+  await playlist.save({ validateBeforeSave: false });
+  return res
+    .status(200)
+    .json(new ApiResponse(200, playlist, "Video removed successfully"));
 });
 
 const deletePlaylist = asyncHandler(async (req, res) => {
