fc9100e82c0bc3f44060f447f81ae3db3c67197e
Kaushik <akaushikarkar@gmail.com>
2024-08-20 19:53:02 +0530

    added infinite scroll and connected backend to fetch videos

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,13 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import VideoCard from "../Video/VideoCard";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Home = () => {
+  const [page, setPage] = useState(1);
+  const [videoList, setVideoList] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    const myFunc = async () => {
+      const resp = await fetch(
+        `http://192.168.31.21:6969/api/v1/videos/?page=${page}&limit=10&sortType=desc&sortBy=createdAt`
+      );
+      const data = await resp.json();
+      setVideoList(videoList.concat(data.data.docs));
+      setHasMore(data.data.hasNextPage);
+    };
+    myFunc();
+  }, []);
+
+  const fetchData = async () => {
+    const resp = await fetch(
+      `http://192.168.31.21:6969/api/v1/videos/?page=${page + 1}&limit=10&sortType=desc&sortBy=createdAt`
+    );
+    setPage(page + 1);
+    const data = await resp.json();
+    setVideoList(videoList.concat(data.data.docs));
+    setHasMore(data.data.hasNextPage);
+  };
+
   return (
-    <div className="w-full min-h-[calc(100vh-4rem)] p-5 flex flex-wrap justify-evenly ">
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
+    <div>
+      <InfiniteScroll
+        dataLength={videoList.length}
+        next={fetchData}
+        hasMore={hasMore}
+        loader={videoList.length ? <Loader /> : <></>}
+        endMessage={<EndMessage />}
+      >
+        <div className="w-full min-h-[calc(100vh-4rem)] p-5 flex flex-wrap justify-evenly ">
+          {!videoList.length ? <Loader /> : <></>}
+          {videoList.map((video, ind) => (
+            <VideoCard
+              key={video._id}
+              title={video.title}
+              thumbnail={video.thumbnail}
+              duration={video.duration}
+              views={video.views}
+              owner={video.owner}
+              createdAt={video.createdAt}
+            />
+          ))}
+        </div>
+      </InfiniteScroll>
+    </div>
+  );
+};
+
+const EndMessage = () => {
+  return (
+    <div className="text-center text-gray-400 mb-5">
+      Congrats!, youv'e reached the end.
+    </div>
+  );
+};
+
+const Loader = () => {
+  return (
+    <div className="mb-5 flex w-full items-center justify-center">
+      <div className="w-10 h-10 border-t-[.5em] border-b-[.5em] border-[.5em] border-t-red-400 border-b-red-400 rounded-full animate-spin"></div>
     </div>
   );
 };
--- frontend/src/components/Video/VideoCard.jsx
+++ frontend/src/components/Video/VideoCard.jsx
@@ -1,40 +1,40 @@
 import React from "react";
 
-const VideoCard = () => {
+const VideoCard = ({ title, thumbnail, duration, views, owner, createdAt }) => {
+  const calculateDuration = (dur) => {
+    const time = Math.floor(dur);
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+  };
   return (
-    <div>
-      <div className="max-w-sm my-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <div className="relative">
-          <img
-            className="rounded-t-lg aspect-video"
-            src="https://flowbite.com/docs/images/blog/image-1.jpg"
-            alt="thumbnail"
-          />
-          <div className="absolute bottom-2 right-2 bg-black bg-opacity-85 text-white text-sm px-2 rounded">
-            3:32
-          </div>
+    <div className="max-w-sm my-2 bg-white border border-gray-200 rounded-lg shadow cursor-pointer dark:bg-gray-800 dark:border-gray-700 hover:shadow-lg hover:scale-[101%] transition-all">
+      <div className="relative">
+        <img
+          className="rounded-t-lg aspect-video"
+          src={thumbnail}
+          alt="thumbnail"
+        />
+        <div className="absolute bottom-2 right-2 bg-black bg-opacity-85 text-white text-sm px-2 rounded">
+          {calculateDuration(duration)}
         </div>
-        <div className="p-1 flex h-[3.6rem]">
-          <div className="w-[14%] aspect-square">
-            <img
-              src="https://avatars.githubusercontent.com/u/87313991?v=4"
-              alt=""
-            />
+      </div>
+      <div className="p-1 flex h-[3.6rem]">
+        <div className="w-[14%] p-1">
+          <img
+            className="rounded-full aspect-square"
+            src={owner.avatar}
+            alt="avatar"
+          />
+        </div>
+        <div className="w-10/12 flex flex-col justify-center">
+          <div className="max-h-4/6 leading-none px-2 flex items-center overflow-hidden">
+            <p className="line-clamp-2">{title}</p>
           </div>
-          <div className="w-10/12">
-            <div className="h-4/6 leading-none px-2 flex items-center overflow-hidden">
-              <p className="line-clamp-2">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-                sapiente minima neque quisquam exercitationem ut assumenda
-                debitis. Voluptatibus nostrum odio ea nam eius ex tempora rem
-                ad, dolor maxime aliquam.
-              </p>
-            </div>
-            <div className="h-2/6 leading-none text-sm px-2 pt-0.5 flex text-gray-600">
-              <div className="w-1/3">@username</div>
-              <div className="w-1/3">2.3k views</div>
-              <div className="w-1/3">1 month ago</div>
-            </div>
+          <div className="h-2/6 leading-none text-sm px-2 pt-0.5 flex text-gray-600">
+            <div className="w-1/3">@{owner.username}</div>
+            <div className="w-1/3">{views} views</div>
+            <div className="w-1/3">1 month ago</div>
           </div>
         </div>
       </div>
