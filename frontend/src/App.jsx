ec24cb69c30c2fef78edd8317acdbf8c4637d019
Kaushik <akaushikarkar@gmail.com>
2024-08-21 20:29:02 +0530

    Added react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,19 @@ import BottomBar from "./components/BottomBar/BottomBar";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
   return (
     <>
       <Navbar />
-      <Home />
+      <RouterProvider router={router} />
       <BottomBar />
       <Footer />
     </>
