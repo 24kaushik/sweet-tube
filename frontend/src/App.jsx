340e5d8ab72bd81fa8efeb68c1eb58672c918963
Kaushik <akaushikarkar@gmail.com>
2024-09-01 20:12:53 +0530

    added alert

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,29 @@ import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Theater from "./components/Theater/Theater";
+import Alert from "./components/Alert/Alert";
+import { useState } from "react";
 
 function App() {
+  const [alert, setAlert] = useState({ content: "", type: "regular" });
+  const showAlert = (content, type="regular") => {
+    setAlert({
+      content: content,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert({ content: "", type: "" });
+    }, 3500);
+  };
+
   return (
     <BrowserRouter>
-      <Navbar />
+      <Navbar showAlert={showAlert}/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/watch/:videoId" element={<Theater />} />
       </Routes>
+      <Alert alert={alert} />
       <BottomBar />
       <Footer />
     </BrowserRouter>
